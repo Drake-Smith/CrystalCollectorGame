81a38c88f10e59aa61b2af776b5a254453988863
Drake Smith <drakesmithjr@gmail.com>
2016-08-19 16:14:54 -0700

    fixed so that random numbers are assigned after every game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,16 +14,6 @@ $(document).ready(function() {
 
 		//assigns random number values for the new game
 		randNum = Math.floor((Math.random() * 102) + 19);
-		pic1 = Math.floor((Math.random() * 13) + 1);
-		pic2 = Math.floor((Math.random() * 13) + 1);
-		pic3 = Math.floor((Math.random() * 13) + 1);
-		pic4 = Math.floor((Math.random() * 13) + 1);
-
-		//assigns the random value of each crystal to its corresponding element id
-		$('#pic1').attr('data-num', pic1);
-		$('#pic2').attr('data-num', pic2);
-		$('#pic3').attr('data-num', pic3);
-		$('#pic4').attr('data-num', pic4);
 
 		//resets score to 0
 		score = 0;
@@ -33,24 +23,57 @@ $(document).ready(function() {
 		$("#losses").html(losses);
 		$("#randNum").html(randNum);
 		$("#score").html(score);
+
+		playGame();
 	}
 
-	newGame();
+	newGame(); 	//initial new game run
 
-	$('.pics').on('click', function(){
-      	score += parseInt($(this).data('num')); //this adds the crystal's random value amount to total score when crystal is clicked
-      	$("#score").text(score); //updates score on screen
+	function setValues(){
 
-      	if (score > randNum) {
+		//assigns random number value to each pic
+		pic1 = Math.floor((Math.random() * 13) + 1);
+		pic2 = Math.floor((Math.random() * 13) + 1);
+		pic3 = Math.floor((Math.random() * 13) + 1);
+		pic4 = Math.floor((Math.random() * 13) + 1);
+
+		//attributes that random num to the picture
+		$('#pic1').data("num", pic1);
+		$('#pic2').data("num", pic2);
+		$('#pic3').data("num", pic3);
+		$('#pic4').data("num", pic4);
+
+		console.log("pic1: " + pic1);
+		console.log("pic2: " + pic2);
+		console.log("pic3: " + pic3);
+		console.log("pic4: " + pic4);
+	}
+
+	//checks if you've won or lost yet
+	function checkScore(currentScore) {
+		if (currentScore > randNum) {
 			alert("You lose! Play again!");
 			losses++;
+			$('.pics').off('click'); //this detaches the event handler to the class pics
 			newGame();
 		}
-		if (score == randNum) {
+
+		if (currentScore == randNum) {
 			alert("You win! Play again!");
 			wins++;
+			$('.pics').off('click'); //this detaches the event handler to the class pics
 			newGame();
 		}
-	});
+	}
+	
+	function playGame(){
+		setValues(); //assign the values to the pics
+		$('.pics').click(function(){
+
+	      	score += $(this).data("num"); //this adds the crystal's random value amount to total score when crystal is clicked
+	      	$("#score").text(score); //updates score on screen
+	      	checkScore(score);
+		});
+	}	
 });
 
